96411739b694f08ca06f6039ef3030b12b1f0f41
Zhekun Zou <charliezou624@gmail.com>
2025-10-05 00:56:59 +0200

    Upload Week-4 excersices

--- Week-4/Exercises/square_fixation.py
+++ Week-4/Exercises/square_fixation.py
@@ -10,8 +10,9 @@ square = stimuli.Rectangle(size=(100, 100), line_width=5)
 
 control.start(subject_id=1)
 
-fixation.present(clear=True, update=True)
 exp.clock.wait(500)
+
+fixation.present(clear=True, update=False)
 
 square.present(clear=False, update=True)
 exp.keyboard.wait()
